572f92062d97a522957e813f997e8c2ed192d1e8
princewilling <princewillingoo@gmail.com>
2022-11-08 22:34:36 +0100

    fix create_user route

--- app/routers/user.py
+++ app/routers/user.py
@@ -13,6 +13,14 @@ router = APIRouter(
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     
+    user_exp = db.query(models.User).filter(models.User.email == user.email).first()
+    
+    if user_exp:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"Email already exist, kindly login"
+        )
+    
     #hash the password - user.password
     hashed_password = utils.hash(user.password)
     user.password = hashed_password
